9dc9819105ac0a4e0f509f45cacd5e35e3b835ec
Xiu Hong <xiuhong.kooi977055@gmail.com>
2021-01-05 12:16:33 +0000

    Complete all examples on C++ template

--- cpp_srcs/compile_time_less_than_n.cpp
+++ cpp_srcs/compile_time_less_than_n.cpp
@@ -8,14 +8,16 @@ template <int N, int M>
 struct LessThanN {
     int value;
     LessThanN() {
-        static_assert(M < N, "error");
+        static_assert(M < N, "type is invalid");
         value = M;
     }
 };
 
 int main() {
-    LessThanN<5, 4> ltf = LessThanN<5,4>();
-    LessThanN<10, 9> ltt = LessThanN<10,9>();
-    cout << ltf.value + ltt.value;
+    const int n = 10;
+    LessThanN<n, 9> ltt = LessThanN<n,9>();
+    cout << ltt.value << endl; 
+    ltt.value = 50;
+    cout << ltt.value;
     return 0;
 }
