f66d891c6b4fea4ac5ef8ed454ea3bcf3d0f1323
Xiu Hong <xiuhong.kooi977055@gmail.com>
2021-02-05 13:08:50 +0000

    Split Pointers and Vectors into different subsections

--- cpp_srcs/pointers_test.cpp
+++ cpp_srcs/pointers_test.cpp
@@ -4,9 +4,7 @@ using namespace std;
 
 
 int main() {
-	int a = 10;
-	int const *const p  = new int(10);
-	int* q = p;
-	cout << *q << endl;
+	int const * const q  = NULL;
+	cout << q << endl;
 	return 0;
 }
